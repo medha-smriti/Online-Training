43b2d806aeec8ea8ee5576d294ae4f33865bc7ce
medha-smriti <67888246+medha-smriti@users.noreply.github.com>
2020-07-14 13:14:53 +0530

    Updating code as per review comments.

--- TwitterDemo/src/main/java/InitTwitter/CreatePost.java
+++ TwitterDemo/src/main/java/InitTwitter/CreatePost.java
@@ -1,35 +1,46 @@
 package InitTwitter;
 
 import com.sun.org.glassfish.gmbal.ParameterNames;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
+import twitter4j.TwitterException;
+import twitter4j.User;
 
 import javax.annotation.PostConstruct;
-import java.io.*;
-import java.util.Properties;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-
+/**
+ * <h1>Tweeting new Posts on timeline</h1>
+ * <p>When a person wants to put new status on their timeline,
+ *  the status is updated with the provided input string
+ *  </p>
+ *
+ *  @author medha.smriti
+ *  @since  14.07.2020
+ *  @version 1.0
+*/
 public class CreatePost {
 
-     Twitter twitter = new TwitterFactory().getInstance();
+    static Twitter twitter = new TwitterFactory().getInstance();
 
-    /*
-     * Posting a status on the timeline
-     * @params = tweet
+    /**
+     * The method takes a string as input and updates it as
+     * the status on the current user's timeline.
      */
-    @ParameterNames("Tweet")
     @PostConstruct
     public static void newPost(String tweet)
     {
         try {
+            User user = twitter.verifyCredentials();
+            System.out.println("Current user who is updating the status is: " + user.getName());
+
             twitter.updateStatus(tweet);
             System.out.println("tweet successful");
         } catch (TwitterException te) {
-            System.out.println("Error Encountered while trying to post: " + te.getStatusCode());
+            System.out.println("Error Encountered while posting on twitter: " + te.getStatusCode())   ;
             System.out.println(te.getMessage());
         }
     }
